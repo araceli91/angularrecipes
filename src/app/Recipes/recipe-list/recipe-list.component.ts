8cc1a45c8d899b7e5ce2a8c52373622211534b7a
Araceli <ara.estruc@gmail.com>
2018-12-04 17:31:23 +0100

    Lista ng for recipes
    funciona ok!

--- src/app/Recipes/recipe-list/recipe-list.component.ts
+++ src/app/Recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "./recipe.model";
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.scss"]
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ricetta: Recipe = new Recipe(
+    "Cacio e Peppe",
+    "Lorem ipsum",
+    "https://www.mangiaebevi.it/wp-content/uploads/2017/10/cacio-e-pepe-e1507299688257.jpg"
+  );
+  recipes: Recipe[] = [
+    new Recipe(
+      "Pizza",
+      "Lorem ipsum",
+      "https://www.bofrost.it/writable/products/images-v2/9146.jpg"
+    ),
+    new Recipe(
+      "Chiffon Cake",
+      "Lorem ipsum",
+      "https://www.tasteofhome.com/wp-content/uploads/2018/05/exps164_MC163887B03_10_24b.jpg"
+    ),
+    new Recipe(
+      "Lasagna",
+      "Lorem ipsum",
+      "https://sanremo.com.au/content/uploads/2017/05/San-Remo-Top-Ten-JAB_7739-1498x1000.jpg"
+    )
+  ];
+  constructor() {}
+  ingredients = {
+    nome: "Aglio",
+    peso: "5g"
+  };
+  ngOnInit() {}
 }
